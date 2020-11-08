1bae5b9eb3428ecfcc0d4421c64a3c6522bde7cd
mis6us <mis6us@icloud.com>
2020-11-08 22:43:19 +0200

    Initial commit

---
+++ src/CarShop/Seat.java
@@ -0,0 +1,21 @@
+package CarShop;
+
+public class Seat extends CarImp implements Sellable{
+    private Double price;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s sells for %.2f", super.getModel(), this.getPrice());
+    }
+
+}
